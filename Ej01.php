92c40f535bb0c0dd96ed134f37bc357e567f282f
Joan <Joan@DESKTOP-MSRTH4Q>
2019-11-25 09:20:56 +0100

    Subida de tamaño

---
+++ Ej01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $integer = 1;
+        $double = 1.1;
+        $string = "uno";
+        $boolean = True;
+
+        echo "Integer: $integer Double $double String $string Boolean $boolean ."
+        ?>
+    </body>
+</html>
